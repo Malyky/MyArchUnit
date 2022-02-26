b17282e0e8cc28dd0f094ea65741922f68f32dbc
Martin <martin@exmaple.com>
2022-02-26 13:07:20 +0100

    Package Dependency (Complex)

--- src/main/java/com/consol/source/any/AnySource.java
+++ src/main/java/com/consol/source/any/AnySource.java
@@ -1,4 +1,7 @@
 package com.consol.source.any;
 
+import com.consol.foo.Foo;
+
 public class AnySource {
+    Foo foo;
 }
--- src/main/java/com/consol/source/two/TwoSource.java
+++ src/main/java/com/consol/source/two/TwoSource.java
@@ -4,5 +4,5 @@ import com.consol.foo.Foo;
 
 public class TwoSource {
 
-    //Foo foo; 4.1.2
+    Foo foo;
 }

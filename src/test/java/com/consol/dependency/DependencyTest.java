ae06724c4383a26fc1ac4634f43be54ebaa16202
Martin Ly-Ky <martin.ly-ky@consol.de>
2022-03-04 11:30:14 +0100

    setup

--- src/test/java/com/consol/dependency/DependencyTest.java
+++ src/test/java/com/consol/dependency/DependencyTest.java
@@ -5,14 +5,9 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
-
-@AnalyzeClasses(packagesOf = MyMainClass.class)
 public class DependencyTest {
 
-/*    @ArchTest
-    public static final ArchRule moreComplexDepenendcy = // TODO*/
+    /*public static final ArchRule moreComplexDepenendcy = // TODO*/
 
 
 

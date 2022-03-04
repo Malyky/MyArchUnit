c27a9255fa9b7908d0749962fb615cf3efd3fd45
Martin Ly-Ky <martin.ly-ky@consol.de>
2022-03-04 11:30:37 +0100

    setup

--- src/test/java/com/consol/dependency/PackageDependencyTest.java
+++ src/test/java/com/consol/dependency/PackageDependencyTest.java
@@ -14,9 +14,6 @@ import com.tngtech.archunit.lang.syntax.elements.ClassesShouldConjunction;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
-
 public class PackageDependencyTest {
 
 

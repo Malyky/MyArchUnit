24699abca6275b478728e6241afca159ceb93591
Martin Ly-Ky <martin.ly-ky@consol.de>
2022-03-07 13:41:34 +0100

    Refactoring

--- src/test/java/com/consol/dependency/PackageDependencyTest.java
+++ src/test/java/com/consol/dependency/PackageDependencyTest.java
@@ -1,6 +1,12 @@
 package com.consol.dependency;
 
+import org.junit.jupiter.api.Test;
+
 public class PackageDependencyTest {
 
+	@Test
+	public void serviceShouldNotAccessContollers() {
+		// todo
+	}
 
 }
